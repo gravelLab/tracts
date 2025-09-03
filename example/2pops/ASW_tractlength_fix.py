b0f2a5c418ebe2b910639b4d97d2beda3131ddec
gonzalez-delgado <javiergonde@gmail.com>
2025-09-03 11:02:45 -0400

    Style plots + save results + update readme

--- example/2pops/ASW_tractlength_fix.py
+++ example/2pops/ASW_tractlength_fix.py
@@ -8,9 +8,9 @@ from tracts.driver import run_tracts
 script_path = Path(sys.argv[0]).resolve()
 script_directory = script_path.parent
 
-script_path = '/path/to/file/'
+script_path = '/path_to_tracts/tracts/example/2pops/'
 
-driver_filename = "sex_biased_fix_two_pulse.yaml"
+driver_filename = "ASW_fix_two_pulse.yaml"
 
 run_tracts(driver_filename = driver_filename, script_dir = script_path)
 
