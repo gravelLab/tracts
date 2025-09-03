06454d89ffd0d62fad7c662e14c180c248969884
gonzalez-delgado <javiergonde@gmail.com>
2025-09-03 14:49:49 -0400

    Fix typo

--- example/2pops/ASW_tractlength_fix.py
+++ example/2pops/ASW_tractlength_fix.py
@@ -10,7 +10,7 @@ script_directory = script_path.parent
 
 script_path = '/path_to_tracts/tracts/example/2pops/'
 
-driver_filename = "ASW_fix_two_pulse.yaml"
+driver_filename = "sex_biased_fix_two_pulse.yaml"
 
 run_tracts(driver_filename = driver_filename, script_dir = script_path)
 
