0a324c9434b5dc446b340d2facce49a29ce23426
sgravel <gravellab@gmail.com>
2025-12-05 12:48:12 -0500

    added 3 pop sex biased inference, in progres

---
+++ example/3pops_sexbiased/ASW_tractlength_3pop_sexbiased_fix.py
@@ -0,0 +1,17 @@
+import sys
+from pathlib import Path
+
+sys.path.append('.')
+
+from tracts.driver import run_tracts
+
+script_path = Path(sys.argv[0]).resolve()
+script_directory = script_path.parent
+
+script_path = Path(sys.argv[0]).resolve()
+script_directory = script_path.parent
+
+driver_filename = "ASW_tractlength_one_pulse.yaml"
+
+run_tracts(driver_filename = driver_filename, script_dir = script_path)
+
