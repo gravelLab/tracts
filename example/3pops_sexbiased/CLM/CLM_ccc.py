80082a30c4ddbaa5a14958035674ecbb5f10bd46
Simon Gravel <sgravel@quinatum.genome.mcgill.ca>
2026-08-04 09:55:36 -0400

    called incorrect yaml in CLM_ccc

--- example/3pops_sexbiased/CLM/CLM_ccc.py
+++ example/3pops_sexbiased/CLM/CLM_ccc.py
@@ -16,7 +16,7 @@ from tracts.driver import run_tracts
 
 script_path = Path(sys.argv[0]).resolve()
    
-driver_filename = "CLM_ccp.yaml"
+driver_filename = "CLM_ccc.yaml"
 
 run_tracts(driver_filename = driver_filename, script_dir = script_path.parent)
 
